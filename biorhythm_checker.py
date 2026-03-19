34415f802f1d7a74da5f087ecfa09ede046f309a
Michael Kramer <pickelmikel@gmail.com>
2026-03-18 21:15:17 -0700

    add surprise

--- biorhythm_checker.py
+++ biorhythm_checker.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title('Biorhythm Checker')
+st.info('Coming soon...until then push the buttons')
+
+def have_ball():
+    st.balloons()
+
+def have_snow():
+    st.snow()
+    
+st.button('Click me!', on_click=have_ball, key='ball')
+st.button('Click me!', on_click=have_snow, key='snow')
