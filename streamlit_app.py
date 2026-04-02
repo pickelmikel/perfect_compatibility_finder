f0cc86c308794d4dbc0c6f2b69fd69c827726364
pickelmikel <88795708+pickelmikel@users.noreply.github.com>
2026-04-02 12:59:58 -0700

    Update disclaimer message in session state

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.session_state.disclaimer = "Disclaimer: Due to the early stage of development, information provided may change without notice."
+st.session_state.disclaimer = "Disclaimer: Due to the early stage of development, the layout may change without notice."
 
 perfect_compat_finder = st.Page('perfect_compatibility_finder.py',
     title='Perfect Compatibility Finder')
